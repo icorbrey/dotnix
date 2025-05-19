201b2dd1a01a6fd61e361de14bb78e2ab1815dd7
Isaac Corbrey <icorbrey@gmail.com>
2025-05-18 20:13:13 -0400

    fonts: Add Recursive

--- modules/home/fonts.nix
+++ modules/home/fonts.nix
@@ -3,12 +3,16 @@
     enable = lib.mkEnableOption "fonts";
 
     fira-code = utils.mkToggle "Fira Code" true;
+    recursive = utils.mkToggle "Recursive" true;
   };
 
   config = let fonts = config.modules.home.fonts;
     in lib.mkIf fonts.enable {
+      fonts.fontconfig.enable = true;
+      
       home.packages = utils.mkIfOptions fonts {
         fira-code = pkgs.fira-code;
+        recursive = pkgs.nerd-fonts.recursive-mono;
       };
     };
 }
