48dae1a1f6c3ffd20926d00c715b9004551e6e4c
Isaac Corbrey <icorbrey@gmail.com>
2025-08-18 11:33:09 +0000

    wezterm: Set up WSL bridge

--- modules/home/wezterm/default.nix
+++ modules/home/wezterm/default.nix
@@ -6,6 +6,13 @@
   config = let wezterm = config.modules.home.wezterm;
     in lib.mkIf wezterm.enable (lib.mkMerge [
       {
+        modules.home.wsl-bridge.map = {
+          "~/.config/wezterm/wezterm.lua" = {
+            directory = { userHome, ... }: "${userHome}/.config/wezterm";
+            filename = "wezterm.lua";
+          };
+        };
+        
         programs.wezterm.enable = true;
         programs.wezterm.extraConfig = builtins.readFile ./wezterm.lua;
       }
