98aa1f841a2febf4963f79247c3503f854b2b254
Isaac Corbrey <icorbrey@gmail.com>
2025-08-18 12:28:16 +0000

    [wip] wezterm: Configure terminal

--- modules/home/wezterm/wezterm.lua
+++ modules/home/wezterm/wezterm.lua
@@ -1,4 +1,13 @@
 local wezterm = require 'wezterm'
 local config = {}
 
+config.font = wezterm.font "Rec Mono Duotone"
+config.font_size = 11.0
+
+config.adjust_window_size_when_changing_font_size = false
+config.window_decorations = "RESIZE"
+config.audible_bell = "Disabled"
+
+config.quick_select_alphabet = "jklfdsauiohnmretcgwvpyqxbz"
+
 return config
