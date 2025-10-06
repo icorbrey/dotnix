da6b17163a8df01347e283a8186d3cf183cce914
Isaac Corbrey <icorbrey@gmail.com>
2025-10-06 12:36:54 -0400

    jujutsu: Add restack alias

--- modules/home/jujutsu/default.nix
+++ modules/home/jujutsu/default.nix
@@ -73,6 +73,7 @@
 
             aliases.jj = [];
             aliases.tug = ["bookmark" "move" "--from" "closest_bookmark(@)" "--to" "closest_pushable(@)"];
+            aliases.restack = ["rebase" "-d" "trunk()" "-s" "roots(trunk()..) & mutable()"];
             aliases.stack = ["rebase" "-A" "trunk()" "-B" "closest_merge(@)" "-r"];
             aliases.stage = ["stack" "closest_merge(@)+:: ~ empty()"];
             aliases.solve = ["resolve" "--tool" "mergiraf"];
