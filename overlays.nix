a34ffdb8d7bc8a2cdf37beaafdc8587242ddc567
Isaac Corbrey <isaac@isaaccorbrey.com>
2026-04-27 21:49:47 -0400

    helix: Fix borked Go lang

--- overlays.nix
+++ overlays.nix
@@ -12,11 +12,32 @@
       ${name} = input.packages.${system}.${package};
     }
     else {};
+
+  helixPackage = final:
+    if
+      inputs.helix ? packages
+      && builtins.hasAttr system inputs.helix.packages
+      && builtins.hasAttr "default" inputs.helix.packages.${system}
+    then let
+      gotmplGrammar = final.runCommand "helix-tree-sitter-gotmpl" {} ''
+        mkdir -p $out
+        ln -s ${final.vimPlugins.nvim-treesitter-parsers.gotmpl}/parser/gotmpl.so $out/gotmpl.so
+      '';
+    in {
+      helix = inputs.helix.packages.${system}.default.override {
+        grammarOverlays = [
+          (_: _: {
+            gotmpl = gotmplGrammar;
+          })
+        ];
+      };
+    }
+    else {};
 in [
   inputs.nur.overlays.default
 
   (final: prev:
     optionalInputPackage "auto-cpufreq" inputs.auto-cpufreq "default"
-    // optionalInputPackage "helix" inputs.helix "default"
+    // helixPackage final
     // optionalInputPackage "tuicr" inputs.llm-agents "tuicr")
 ]
